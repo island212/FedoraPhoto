fc75d5d1741514c09aa6f1271d05e6b3a8523bf5
Wojtek <j.wojtek.thompson@gmail.com>
2015-05-05 17:12:49 -0400

    PhotographeRepository and shiet

---
+++ FedoraPhoto/FedoraPhoto/DAL/PhotographeRepository.cs
@@ -0,0 +1,33 @@
+﻿using FedoraPhoto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FedoraPhoto.DAL
+{
+    public class PhotographeRepository : GenericRepository<Photographe>
+    {
+        public PhotographeRepository(Model1 context) : base(context) { }
+
+        public IEnumerable<Photographe> ObtenirPhotographes()
+        {
+            return Get();
+        }
+
+        public IEnumerable<Photographe> ObtenirPhotographesTries()
+        {
+            return Get(orderBy: a => a.OrderByDescending(al => al.Nom));
+        }
+
+        public Photographe ObtenirPhotographeParID(int? id)
+        {
+            return GetByID(id);
+        }
+
+        public void InsererPhotographe(Photographe photographe)
+        {
+            Insert(photographe);
+        }
+    }
+}
