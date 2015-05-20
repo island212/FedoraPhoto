79e29c96630e3518b113e8709635283e438b0582
Calvin263 <marcoarseneault263@gmail.com>
2015-05-20 16:10:27 -0400

    * Obligatoires part 1

---
+++ FedoraPhoto/FedoraPhoto/Models/AgentMetadata.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.Spatial;
+
+namespace FedoraPhoto.Models
+{
+
+    [MetadataType(typeof(AgentMetadata))]
+    public partial class Agent
+    {
+        [Display(Name = "Nom Agent")]
+        public string Nom { get; set; }
+
+        [Display(Name = "Prénom Agent")]
+        public string Prenom { get; set; }
+
+        [Display(Name = "Téléphone Agent")]
+        public string Telephone { get; set; }
+    }
+}
