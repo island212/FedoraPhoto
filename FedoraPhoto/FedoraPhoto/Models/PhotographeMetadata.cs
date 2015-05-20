749945cd77ca38afc5b619bf3c03054a45aa63e4
Calvin263 <marcoarseneault263@gmail.com>
2015-05-20 16:45:07 -0400

    * Obligatoires part 2

---
+++ FedoraPhoto/FedoraPhoto/Models/PhotographeMetadata.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FedoraPhoto.Models
+{
+    [MetadataType(typeof(PhotographeMetadata))]
+    public partial class Photographe
+    {
+        internal sealed class PhotographeMetadata
+        {
+            [Display(Name = "Nom Photographe")]
+            public string Nom { get; set; }
+
+            [Display(Name = "Prénom Photographe")]
+            public string Prenom { get; set; }
+        }
+    }
+}
